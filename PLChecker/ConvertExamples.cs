67dc2d9bdfc1cd9a4348f6b80db18225e287b517
Oliver Michaelsen <omicha19@student.aau.dk>
2022-11-19 12:37:14 +0100

    Add project files.

---
+++ PLChecker/ConvertExamples.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLChecker
+{
+    class ConvertExamples
+    {
+        public void writeExamples()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:/Users/micha/Desktop/examples/exs.pl");
+            List<string> examples = new List<string>();
+
+            foreach (var line in lines)
+            {
+                if (line.Contains("pos") || line.Contains("neg"))
+                {
+                    string result = line.Remove(0, 4);
+                    examples.Add(result);
+                }
+            }
+
+            using (StreamWriter writeFile = new StreamWriter("C:/Users/micha/Desktop/examples/positiveExs.pl"))
+            {
+
+            }
+
+        }
+    }
+}
