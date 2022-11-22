21018e991e1376b2a171d05f40a67127432a31e3
Oliver Michaelsen <omicha19@student.aau.dk>
2022-11-22 09:58:15 +0100

    Read examples

--- PLChecker/Program.cs
+++ PLChecker/Program.cs
@@ -10,7 +10,21 @@ class Program
         ConvertExamples convert = new ConvertExamples();
         convert.writeExamples();
 
+        //read examples file
+        ReadExamples read = new ReadExamples();
+        List<string> examples = read.Read();
+
+        var prolog = new PrologEngine(persistentCommandHistory: false);
+        prolog.Consult("C:\\Users\\micha\\Desktop\\PLChecker\\PLChecker\\input\\domain.pl");
+
+        foreach (var example in examples)
+        {
+
+        }
 
 
+        var solution = prolog.GetFirstSolution(query: "move_t_to_b(b5,b8,p48).");
+
+        Console.WriteLine(solution.Solved);
     }
 }
---
+++ PLChecker/ReadExamples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLChecker
+{
+    class ReadExamples
+    {
+        public List<string> Read()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\micha\Desktop\PLChecker\PLChecker\input\examples.pl");
+            List<string> examples = new List<string>();
+
+            foreach (string line in lines)
+            {
+                examples.Add(line);
+            }
+
+            return examples;
+        }
+    }
+}
